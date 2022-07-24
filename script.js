49067bea3d900bad2a96262022f7a7824532404f
Aliyu Abubakar <84377921+Retro-xd@users.noreply.github.com>
2022-07-24 22:00:14 +0100

    updated

--- script.js
+++ script.js
@@ -1,58 +1,32 @@
-const menuToggler = document.querySelector('.menu-toggler')
-const navbar = document.querySelector('.navbar')
-const collapsibleMenu = document.querySelector('.collapsible-menu')
+const menuToggler = document.querySelector('.menu-toggler');
+const navbar = document.querySelector('.navbar');
+const collapsibleMenu = document.querySelector('.collapsible-menu');
 
-const faqList = document.querySelector('.faq-list')
-const faqListItem = document.querySelector('.faq-list-item')
-const dropdown = document.querySelector('.dropdown')
-const faqListText = document.querySelector('.faq-list-text')
+let popup = document.querySelector('.popup');
+let overlay = document.querySelector('.overlay');
 
-const faq1 = document.querySelector('.faq-1')
-const faq2 = document.querySelector('.faq-2')
-const faq3 = document.querySelector('.faq-3')
-const faq4 = document.querySelector('.faq-4')
+const faqListItems = document.querySelectorAll('.faq-list-item');
 
-const dropdown1 = document.querySelector('.dropdown-1')
-const dropdown2 = document.querySelector('.dropdown-2')
-const dropdown3 = document.querySelector('.dropdown-3')
-const dropdown4 = document.querySelector('.dropdown-4')
-
-let popup = document.querySelector('.popup')
-let overlay = document.querySelector('.overlay')
-
+faqListItems.forEach((element) => {
+  element.addEventListener('click', () => {
+    element.classList.toggle('faq-show');
+  });
+});
 
 // Menu Toggle
 
-menuToggler.addEventListener ('click', () => {
-    navbar.classList.toggle ('show-menu')
-})
+menuToggler.addEventListener('click', () => {
+  navbar.classList.toggle('show-menu');
+});
 
-// Faq List
+// // Popup
 
-dropdown1.addEventListener('click', () => {
-    faq1.classList.toggle('activated')
-})
-
-dropdown2.addEventListener('click', () => {
-    faq2.classList.toggle('activated')
-})
-
-dropdown3.addEventListener('click', () => {
-    faq3.classList.toggle('activated')
-})
-
-dropdown4.addEventListener('click', () => {
-    faq4.classList.toggle('activated')
-})
-
-// Popup
-
-function openPopup(){
-    popup.classList.add("activated")
-    overlay.classList.add("activated")
+function openPopup() {
+  popup.classList.add('activated');
+  overlay.classList.add('activated');
 }
 
-function closePopup(){
-    popup.classList.remove("activated")
-    overlay.classList.remove("activated")
+function closePopup() {
+  popup.classList.remove('activated');
+  overlay.classList.remove('activated');
 }
